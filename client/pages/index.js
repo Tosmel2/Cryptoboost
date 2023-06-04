6e050131a93896e323738ecd9e7939c8f7274ca6
tosmel2 <adewaletosin0808@gmail.com>
2023-06-04 23:43:25 +0100

    refactor: footer component and code refactor

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,73 +1,74 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className='h-full w-full bg-slate-600'>
-      <section className='w-[90%] mx-auto'>
-        <div className='text-start text-white p-10'>
+      <section className='funding-projects md:py-10  w-[90%] mx-auto'>
+        <div className='text-start text-white p-10 md:px-0'>
         <h1 className='text-4xl font-bold'>Funded Projects</h1>
         <p className='text-lg '>Succesful projects that have been funded through the platform</p>
         </div>
-        <div className='projects-container'>
+        <div className='projects-container md:flex md:justify-between md:items-center w-full md:flex-wrap'>
 
-          <div className='project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
@@ -76,109 +77,111 @@ export default function Home() {
         </div>
       </section>
 
-      <section className='w-[90%] mx-auto'>
+      <section className='testimonial w-[90%]  mx-auto'>
         <div className='testimonial-header text-center text-white py-8 px-0'>
           <h1 className='text-4xl font-bold'>Testimonials</h1>
           <p className='text-lg '>Hear from the horses mouth and be inspired</p>
         </div>
 
-        <div className='h-[35vh] bg-teal-500 mb-5 rounded-xl w-full border border-solid border-white'>
-          <div className='h-[20%] flex justify-between items-center'>
-            <p className='text-lg text-center text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-            </p>
-          </div>
-            <div className='h-[80%] w-full md:w-1/2'>
-              <h1 className='text-2xl font-bold text-center text-white'>Just the support I needed</h1>
-              <p className='text-lg text-center text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        <div className='md:flex md:justify-between md:items-center md:h-[40vh]'>
+          <div className='md:w-[32%] p-4 leading-6 bg-teal-500 mb-5 rounded-xl w-full border border-solid border-white'>
+            <div className='h-[20%] flex justify-between items-center'>
+              <p className='text-lg text-center md:text-start text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+              </p>
             </div>
-        </div>
-
-        <div className='h-[35vh] bg-teal-500 mb-5 rounded-xl w-full border border-solid border-white'>
-          <div className='h-[20%] flex justify-between items-center'>
-            <p className='text-lg text-center text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-            </p>
-          </div>
-            <div className='h-[80%] w-full md:w-1/2'>
-              <h1 className='text-2xl font-bold text-center text-white'>Just the support I needed</h1>
-              <p className='text-lg text-center text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            <div className='h-[80%] w-full md:text-start md:mt-4'>
+              <h1 className='text-xl font-bold text-center md:text-start text-white'>Just the support I needed</h1>
+              <p className='text-medium text-center md:text-start text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
-        </div>
-
-        <div className='h-[35vh] bg-teal-500 mb-5 rounded-xl w-full border border-solid border-white'>
-          <div className='h-[20%] flex justify-between items-center'>
-            <p className='text-lg text-center text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-            </p>
           </div>
-            <div className='h-[80%] w-full md:w-1/2'>
-              <h1 className='text-2xl font-bold text-center text-white'>Just the support I needed</h1>
-              <p className='text-lg text-center text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-            </div>
-        </div>
 
+          <div className='md:w-[32%] p-4 leading-6 bg-teal-500 mb-5 rounded-xl w-full border border-solid border-white'>
+            <div className='h-[20%] flex justify-between items-center'>
+              <p className='text-lg text-center md:text-start text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+              </p>
+            </div>
+            <div className='h-[80%] w-full md:text-start md:mt-4'>
+              <h1 className='text-xl font-bold text-center md:text-start text-white'>Just the support I needed</h1>
+              <p className='text-medium text-center md:text-start text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+          </div>
+
+          <div className='md:w-[32%] p-4 leading-6 bg-teal-500 mb-5 rounded-xl w-full border border-solid border-white'>
+            <div className='h-[20%] flex justify-between items-center'>
+              <p className='text-lg text-center md:text-start text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+              </p>
+            </div>
+            <div className='h-[80%] w-full md:text-start md:mt-4'>
+              <h1 className='text-xl font-bold text-center md:text-start text-white'>Just the support I needed</h1>
+              <p className='text-medium text-center md:text-start text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+          </div>
+
+        </div>
       </section>
 
-      <section className='w-[90%] mx-auto'>
+      <section className='ongoing-projects w-[90%] mx-auto md:py-10'>
         <div className='text-center text-white py-8 px-0'>
         <h1 className='text-4xl font-bold'>Ongoing Projects</h1>
         <p className='text-lg '>Lists of ongoing projects to invest in.</p>
         </div>
-        <div className='ong-projects-container'>
+        <div className='ong-projects-container md:flex md:justify-between md:items-center w-full md:flex-wrap'>
 
-          <div className='ong-project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='ong-project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='ong-project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='ong-project-card-image rounded-xl md:rounded-b-none -xl h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='ong-project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='ong-project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='ong-project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='ong-project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='ong-project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='ong-project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='ong-project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='ong-project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='ong-project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='ong-project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='ong-project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='ong-project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='ong-project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='ong-project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='ong-project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='ong-project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='ong-project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='ong-project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
           </div>
 
-          <div className='ong-project-card mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
-            <div className='project-card-image rounded-xl h-2/5 w-full bg-slate-800'>
+          <div className='ong-project-card md:w-[32%] mb-5 rounded-xl w-full h-[50vh] border border-solid border-white'>
+            <div className='project-card-image rounded-xl md:rounded-b-none h-2/5 w-full bg-slate-800'>
               {/* <Image src='./assets/images/solana.png' className='w-full h-full object-cover rounded-br-md rounded-bl-md' /> */}
             </div>
-            <div className='ong-project-card-content bg-teal-500 px-5 h-3/5 w-full'>
+            <div className='ong-project-card-content rounded-b-xl bg-teal-500 px-5 h-3/5 w-full'>
               <h1 className='text-2xl font-bold'>Project Title</h1>
               <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
             </div>
@@ -186,6 +189,8 @@ export default function Home() {
 
         </div>
       </section>
+
+      <Footer />
 
     </main>
   )
--- client/pages/investors/signup.jsx
+++ client/pages/investors/signup.jsx
@@ -3,18 +3,15 @@ import React from 'react'
 const SignUp = () => {
   return (
     <>
-    <main className='h-full bg-teal-700 flex flex-col justify-center items-center'>
+    <main className='h-full bg-teal-700 py-10 flex flex-col justify-center items-center'>
       <div className="w-full max-w-md p-4 rounded-md shadow sm:p-8">
       <h2 className="mb-3 text-2xl font-bold text-white text-center uppercase">Create your account</h2>
-      <p className="text-sm text-center text-white mb-8">Dont have account?
-        <a href="#" rel="noopener noreferrer" className="focus:underline hover:underline">Sign up here</a>
-      </p>
 
       <form novalidate="" action="" className="space-y-8 ng-untouched ng-pristine ng-valid mb-12">
         <div className="space-y-4">
           <div className="space-y-2">
-            <label for="email" className="block text-md font-bold text-white">Email address</label>
-            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
+            <label for="email" className="block text-md font-bold text-white">Full Name</label>
+            <input type="text" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
           </div>
 
           <div className="space-y-2">
@@ -23,29 +20,32 @@ const SignUp = () => {
           </div>
 
           <div className="space-y-2">
-            <label for="email" className="block text-md font-bold text-white">Email address</label>
-            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
+            <div className="flex justify-between">
+              <label for="password" className="text-md font-bold text-white">Password</label>
+            </div>
+            <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-4 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
           </div>
 
           <div className="space-y-2">
-            <label for="email" className="block text-md font-bold text-white">Email address</label>
-            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
+            <label for="email" className="block text-md font-bold text-white">Wallet address for cryptocurrency</label>
+            <input type="text" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
           </div>
 
           <div className="space-y-2">
-            <label for="email" className="block text-md font-bold text-white">Email address</label>
-            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
+            <label for="email" className="block text-md font-bold text-white">Country of Residence</label>
+            <input type="text" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
           </div>
 
           <div className="space-y-2">
-            <label for="email" className="block text-md font-bold text-white">Email address</label>
-            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
+            <label for="number" className="block text-md font-bold text-white">Phone number</label>
+            <input type="number" name="pnumber" id="pnumber" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
           </div>
 
-          <div className="space-y-2">
-            <label for="email" className="block text-md font-bold text-white">Email address</label>
-            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-4 border rounded-md" />
-          </div>
+         <div>
+          <p className='text-white'>By creating an account you accept the {''}
+            <a href="/" rel="noopener noreferrer" className='text-white font-bold underline'>Terms of Services and Privacy Policy</a>
+          </p>
+         </div>
           
         </div>
         <button type="button" className="w-full px-8 py-3 font-semibold rounded-md bg-teal-400 text-white">Sign in</button>
