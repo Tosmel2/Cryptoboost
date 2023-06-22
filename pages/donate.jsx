c42996a2d7997bb2964f3f04f4d9593e34325715
tosmel2 <adewaletosin0808@gmail.com>
2023-06-23 00:05:18 +0100

    donate page fix

--- pages/donate.jsx
+++ pages/donate.jsx
@@ -10,22 +10,22 @@ const Donate = () => {
           <div className='font-bold text-purple-700 text-2xl border-b-2 mb-3 pb-2'>Donate: Bromuno</div>
           <div className="space-y-4">
             <div className="space-y-1">
-              <label htmlFor="number" className="block text-sm font-bold text-pink-600">Currency</label>
-              <input type="number" name="number" placeholder="Select crypto" className="bg-orange-50 w-full p-2 border rounded-3xl focus:outline-none pl-4 " />
+              <label htmlFor="number" className="block text-sm font-bold text-purple-600">Currency</label>
+              <input type="number" name="number" placeholder="Select crypto" className="bg-purple-50 w-full p-2 border rounded-3xl focus:outline-none pl-4 " />
             </div>
 
             <div className="space-y-1">
-              <label htmlFor="number" className="block text-sm font-bold text-pink-600">Amount <span className='text-gray-400'>(optional)</span> </label>
-              <input type="number" name="number" id="number" placeholder="Enter amount" className="bg-orange-50 w-full p-2 border rounded-3xl focus:outline-none pl-4 " />
+              <label htmlFor="number" className="block text-sm font-bold text-purple-600">Amount <span className='text-gray-400'>(optional)</span> </label>
+              <input type="number" name="number" id="number" placeholder="Enter amount" className="bg-purple-50 w-full p-2 border rounded-3xl focus:outline-none pl-4 " />
             </div>
 
             <div className="space-y-1">
-              <label htmlFor="message" className="block text-sm font-bold text-pink-600">Remark</label>
-              <textarea type="text" name="message" rows={10} cols={20} placeholder="Tell us what you feel about the project" className="bg-orange-50 w-full p-2 border rounded-lg focus:outline-none pl-4 resize-none " />
+              <label htmlFor="message" className="block text-sm font-bold text-purple-600">Remark</label>
+              <textarea type="text" name="message" rows={10} cols={20} placeholder="Tell us what you feel about the project" className="bg-purple-50 w-full p-2 border rounded-lg focus:outline-none pl-4 resize-none " />
             </div>
 
             <div className='pt-2'>
-            <button type="button" className="md:w-[60%] w-full px-8 py-2 font-semibold rounded-3xl bg-pink-700 text-white">Send message</button>
+            <button type="button" className="md:w-[60%] w-full px-8 py-2 font-semibold rounded-3xl bg-purple-700 text-white">Send message</button>
             </div>
             
           </div>
