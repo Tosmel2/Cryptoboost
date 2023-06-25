101bb2f2513c82dee58960ac13d1d0de9123ce24
3illBaby <thrillanon@outlook.com>
2023-06-25 02:37:14 +0100

    upated the abi

--- utils/abi.js
+++ utils/abi.js
@@ -1,158 +1,198 @@
 export const abi = [
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: "address",
-        name: "_owner",
-        type: "address",
-      },
+        "indexed": true,
+        "internalType": "address",
+        "name": "_owner",
+        "type": "address"
+      }
     ],
-    name: "campaignCreated",
-    type: "event",
+    "name": "campaignCreated",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: "address",
-        name: "_donor",
-        type: "address",
+        "indexed": true,
+        "internalType": "address",
+        "name": "_donor",
+        "type": "address"
       },
       {
-        indexed: false,
-        internalType: "string",
-        name: "_title",
-        type: "string",
+        "indexed": false,
+        "internalType": "string",
+        "name": "_title",
+        "type": "string"
       },
       {
-        indexed: false,
-        internalType: "uint256",
-        name: "_amount",
-        type: "uint256",
-      },
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
     ],
-    name: "donatedTo",
-    type: "event",
+    "name": "donatedTo",
+    "type": "event"
   },
   {
-    stateMutability: "payable",
-    type: "fallback",
+    "stateMutability": "payable",
+    "type": "fallback"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
     ],
-    name: "campaigns",
-    outputs: [
+    "name": "campaigns",
+    "outputs": [
       {
-        internalType: "address",
-        name: "campaignOwner",
-        type: "address",
+        "internalType": "address",
+        "name": "campaignOwner",
+        "type": "address"
       },
       {
-        internalType: "string",
-        name: "campaignTitle",
-        type: "string",
+        "internalType": "string",
+        "name": "campaignTitle",
+        "type": "string"
       },
       {
-        internalType: "string",
-        name: "campaignDescription",
-        type: "string",
+        "internalType": "string",
+        "name": "campaignDescription",
+        "type": "string"
       },
       {
-        internalType: "uint256",
-        name: "amountRealised",
-        type: "uint256",
-      },
+        "internalType": "uint256",
+        "name": "amountRealised",
+        "type": "uint256"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "string",
-        name: "_campaignTitle",
-        type: "string",
+        "internalType": "string",
+        "name": "_campaignTitle",
+        "type": "string"
       },
       {
-        internalType: "string",
-        name: "_campaignDescription",
-        type: "string",
-      },
+        "internalType": "string",
+        "name": "_campaignDescription",
+        "type": "string"
+      }
     ],
-    name: "createCampaign",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    "name": "createCampaign",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "_campaignId",
-        type: "uint256",
-      },
+        "internalType": "uint256",
+        "name": "_campaignId",
+        "type": "uint256"
+      }
     ],
-    name: "donate",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
+    "name": "donate",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "getAllDonors",
-    outputs: [
+    "inputs": [],
+    "name": "getAllCampaigns",
+    "outputs": [
       {
-        internalType: "address[][]",
-        name: "",
-        type: "address[][]",
-      },
+        "components": [
+          {
+            "internalType": "address",
+            "name": "campaignOwner",
+            "type": "address"
+          },
+          {
+            "internalType": "string",
+            "name": "campaignTitle",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "campaignDescription",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "amountRealised",
+            "type": "uint256"
+          },
+          {
+            "internalType": "address[]",
+            "name": "campaignDonors",
+            "type": "address[]"
+          }
+        ],
+        "internalType": "struct CryptoBoost.Campaign[]",
+        "name": "",
+        "type": "tuple[]"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [],
+    "name": "getAllDonors",
+    "outputs": [
       {
-        internalType: "uint256",
-        name: "_campaignId",
-        type: "uint256",
-      },
+        "internalType": "address[][]",
+        "name": "",
+        "type": "address[][]"
+      }
     ],
-    name: "getDonorsOfCampaign",
-    outputs: [
-      {
-        internalType: "address[]",
-        name: "",
-        type: "address[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "_campaignId",
-        type: "uint256",
-      },
+        "internalType": "uint256",
+        "name": "_campaignId",
+        "type": "uint256"
+      }
     ],
-    name: "withdraw",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
+    "name": "getDonorsOfCampaign",
+    "outputs": [
+      {
+        "internalType": "address[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    stateMutability: "payable",
-    type: "receive",
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_campaignId",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
   },
-];
+  {
+    "stateMutability": "payable",
+    "type": "receive"
+  }
+]
