4659149edfbd8f20b9120c55b7b085dcf187e6d0
3illBaby <thrillanon@outlook.com>
2023-06-25 04:00:23 +0100

    added the getAllCampaign function

---
+++ utils/getAllCampaigns.js
@@ -0,0 +1,44 @@
+import { ethers } from "ethers";
+import { abi } from "./abi";
+
+const contractAddress = "0x45989d3583f0ac991753bacda42dbc4f46eb3f2b";
+
+const getAllCampaigns = async () => {
+  try {
+    console.log("Executing function");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    const cryptoBoost = new ethers.Contract(contractAddress, abi, signer);
+    const data = await cryptoBoost.getAllCampaigns();
+
+    const dataString = String(data);
+
+    const tuples = dataString.split(",");
+
+    const objects = [];
+    for (let i = 0; i < tuples.length; i += 5) {
+      const address = tuples[i].trim();
+      const title = tuples[i + 1].trim();
+      const description = tuples[i + 2].trim();
+      const amountFunded = parseInt(tuples[i + 3].trim());
+      const donors = tuples[i + 4].split(",").map((item) => item.trim());
+
+      objects.push({
+        address,
+        title,
+        description,
+        amountFunded,
+        donors,
+      });
+    }
+
+    const campaignData = JSON.stringify(objects);
+    console.log(campaignData);
+    return campaignData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getAllCampaigns };
